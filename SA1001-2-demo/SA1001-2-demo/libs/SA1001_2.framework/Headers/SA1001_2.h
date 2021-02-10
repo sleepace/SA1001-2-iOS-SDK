173b59a32a25e1ab78d0b64e1e58548075338e04
Michael <micheal@sleepace.net>
2021-02-10 10:30:40 +0800

    install project

---
+++ SA1001-2-demo/SA1001-2-demo/libs/SA1001_2.framework/Headers/SA1001_2.h
@@ -0,0 +1,14 @@
+//
+//  SA1001_2.h
+//  SA1001_2
+//
+//  Created by Martin on 12/11/18.
+//  Copyright © 2018年 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLPSA1001_2Def.h"
+#import "SLPBLEManager+SA1001_2.h"
+
+#define kSA10012Version  @"3.0.0";
